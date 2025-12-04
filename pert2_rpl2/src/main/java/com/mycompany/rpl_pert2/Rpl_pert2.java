de6013fb1c5954739a7f266a007bb0530d948d5b
Khansa Rifli <khansa.fr9@gmail.com>
2025-12-04 21:35:19 +0700

    Upload beberapa folder

---
+++ pert2_rpl2/src/main/java/com/mycompany/rpl_pert2/Rpl_pert2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rpl_pert2;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Rpl_pert2 {
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.login();
+        user.logout();
+        
+        Seller seller = new Seller();
+        seller.addProduct("Nasi Goreng");
+        
+        Buyer buyer = new Buyer();
+        buyer.login();
+        buyer.logout();
+        
+        Admin admin = new Admin();
+        admin.login();
+        admin.manageUser();
+        admin.logout();
+    }
+
+}
